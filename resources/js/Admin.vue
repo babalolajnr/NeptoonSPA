cec10123a8a7e278d0661ab35fe19e42aaec9a23
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-09-21 21:55:27 +0100

    ...

--- resources/js/Admin.vue
+++ resources/js/Admin.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!--navbar-->
+        <!--navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
