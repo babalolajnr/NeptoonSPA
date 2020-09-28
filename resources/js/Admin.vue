ccd4f81117f5fbe20c8cd2480992ffd85f23f2b1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-09-28 10:47:46 +0100

    ❌removed the useless elements from the dashboard component

--- resources/js/Admin.vue
+++ resources/js/Admin.vue
@@ -237,18 +237,6 @@
                             </span>
 
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon far fa-circle text-warning"></i>
-                                <p>Warning</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon far fa-circle text-info"></i>
-                                <p>Informational</p>
-                            </a>
-                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
